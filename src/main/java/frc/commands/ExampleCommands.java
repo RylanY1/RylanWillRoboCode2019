542117a9a32875ebccc5b149283105d483431b45
William Lee <23willl@students.harker.org>
2019-10-10 17:30:01 -0700

    Massive changes to file location
    you put them in the wrong folders and i fixed all or at least most of that

--- src/main/java/frc/commands/ExampleCommands.java
+++ src/main/java/frc/commands/ExampleCommands.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
--- src/main/java/frc/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/subsystems/ExampleSubsystem.java
@@ -15,8 +15,6 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ExampleSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  
-  private Talon
 
   @Override
   public void initDefaultCommand() {

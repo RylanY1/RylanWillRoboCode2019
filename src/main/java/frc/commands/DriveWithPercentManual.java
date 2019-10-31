a8765bb89391a68e57b1f541d84926f12db59f70
RylanY1 <23Rylany@students.harker.org>
2019-10-31 16:47:42 -0700

    did absolutely nothing
    did absolutely nothing

--- src/main/java/frc/commands/DriveWithPercentManual.java
+++ src/main/java/frc/commands/DriveWithPercentManual.java
@@ -25,11 +25,6 @@ public class DriveWithPercentManual extends IndefiniteCommand {
   protected void execute() {
   }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
 
   // Called once after isFinished returns true
   @Override
